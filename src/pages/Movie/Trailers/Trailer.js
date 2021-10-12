23a31f8bbe82578580db6a2771204d0c3605706c
Okorie Emmanuel <teymee17@gmail.com>
2021-10-12 18:47:12 +0100

    default screen

--- src/pages/Movie/Trailers/Trailer.js
+++ src/pages/Movie/Trailers/Trailer.js
@@ -6,25 +6,13 @@ function Trailer(props) {
   
   return (
     <div className="trailer" allowfullscreen="allowfullScreen">
+
       <iframe
-       
         src={`https://www.youtube.com/embed/${props.youtube}`}
-        // allowFullScreen="allowfullScreen"
-        // mozallowfullscreen="mozallowfullscreen" 
-        // msallowfullscreen="msallowfullscreen" 
-        // oallowfullscreen="oallowfullscreen" 
-        // webkitallowfullscreen="webkitallowfullscreen"
         title={props.title}
         frameBorder="0"
-        // autoPlay={false}
         allow="accelerometer; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-        // allowFullScreen
-        
       ></iframe>
-
-
-
-
 
     </div>
   );
